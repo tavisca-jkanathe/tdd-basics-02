086d123b5e08433f912cacc146936ba0c03e4f38
tavisca-jkanathe <51927665+tavisca-jkanathe@users.noreply.github.com>
2019-07-05 00:54:27 +0530

    Update CalculatorFixture.cs

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -10,5 +10,40 @@ namespace ConsoleCalculator.Tests
         {
             return;
         }
+        [Fact]
+            public void BasicOperationTest()
+            {
+                Assert.Equal("12",SolveEquation("10+2="));
+            }
+         [Fact]
+            public void BasicOperationTest()
+            {
+                Assert.Equal("-E-",SolveEquation("10/0="));
+            }
+         [Fact]
+            public void BasicOperationTest()
+            {
+                Assert.Equal("0.001",SolveEquation("00..001"));
+            }
+        [Fact]
+            public void BasicOperationTest()
+            {
+                Assert.Equal("10",SolveEquation("12+2sss="));
+            }
+         [Fact]
+            public void BasicOperationTest()
+            {
+                Assert.Equal("0",SolveEquation("1+2+c"));
+            }
     }
+    public string SolveEquation(string equation)
+        {
+            Calculator calculator = new Calculator();
+            string output = "";
+            foreach (char ch in equation)
+            {
+                output = calculator.SendKeyPress(ch);
+            }
+            return output;
+        }
 }
